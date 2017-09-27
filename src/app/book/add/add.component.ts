37e2be38229537c3d03186812795c9bd640bffe5
vladi99 <vlpetrovic99@gmail.com>
2017-09-27 21:19:44 +0300

    add validations

--- src/app/book/add/add.component.ts
+++ src/app/book/add/add.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Book} from '../../models/book.model';
 import {BookService} from '../../services/book.service';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -11,15 +12,41 @@ import {Router} from '@angular/router';
 export class AddComponent implements OnInit {
   title = 'Add Book';
   book: Book = new Book();
+  bookForm: FormGroup;
+
+  minPrice = 1;
+  maxPrice = 100;
+
+  minRating = 1;
+  maxRating = 5;
+
   constructor(
     private bookService: BookService,
     private appRouter: Router) {
   }
 
   ngOnInit() {
+    this.createForm();
   }
 
-  onSubmit(): void {
+  private createForm() {
+    this.bookForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      author: new FormControl('', Validators.required),
+      price: new FormControl('', [
+        Validators.required,
+        Validators.min(this.minPrice),
+        Validators.max(this.maxPrice)]
+      ),
+      rating: new FormControl('', [
+        Validators.required,
+        Validators.min(this.minRating),
+        Validators.max(this.maxRating)]
+      ),
+    });
+  }
+
+  addBook(): void {
     this.bookService.addBook(this.book)
       .subscribe((res: Book) => {
         console.log(res);
@@ -28,4 +55,5 @@ export class AddComponent implements OnInit {
         console.log(err);
       });
   }
+
 }
