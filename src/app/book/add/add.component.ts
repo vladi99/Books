360877c4e8e57d614e7bf25f8bccc993b01ab06e
vladi99 <vlpetrovic99@gmail.com>
2017-10-02 18:07:33 +0300

    Fix responsive and list view (#3)
    * fix showing data
    
    * finish list books
    
    * add book form
    
    * now adding books working, start validation part
    
    * add validations
    
    * finish form
    
    * remove line
    
    * refactor component and start responsivness with flex
    
    * Make form responsive
    
    * fix url
    
    * make with cards, ps:fix design
    
    * make list with cards
    
    * fix form

---
+++ src/app/book/add/add.component.ts
@@ -0,0 +1,57 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Book} from '../../models/book.model';
+import {BookService} from '../../services/book.service';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+  title = 'Add Book';
+  book: Book = new Book();
+  bookForm: FormGroup;
+
+  minPrice = 1;
+  maxPrice = 100;
+
+  minRating = 1;
+  maxRating = 5;
+
+  constructor(@Inject(FormBuilder) fb: FormBuilder,
+    private bookService: BookService,
+    private appRouter: Router) {
+    this.bookForm = fb.group({
+      name: [this.book.Name, Validators.required],
+      author: [this.book.Author, Validators.required],
+      price: [this.book.Price, [
+        Validators.required,
+        Validators.min(this.minPrice),
+        Validators.max(this.maxPrice)]
+      ],
+      rating: [this.book.Rating, [
+        Validators.required,
+        Validators.min(this.minRating),
+        Validators.max(this.maxRating)]
+      ],
+      pictureUrl: this.book.PictureURL,
+      description: this.book.Description
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  addBook(): void {
+    this.bookService.addBook(this.bookForm.value)
+      .subscribe((res: Book) => {
+        console.log(res);
+        this.appRouter.navigateByUrl('books');
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+}
--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,8 +1,9 @@
 export class Book {
-  id: number;
-  name: string;
-  author: string;
-  pictureUrl: string;
-  price: number;
-  rating: number;
+  Id: number;
+  Name: string;
+  Author: string;
+  PictureURL: string;
+  Price: number;
+  Rating: number;
+  Description: string;
 }
