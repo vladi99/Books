8e1f8c6c9f5d43a46d7b3e918f49ef0498a4eba2
vladi99 <vlpetrovic99@gmail.com>
2017-10-03 13:00:01 +0300

    finish single view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  Event as RouterEvent
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loading = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 }
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -12,11 +12,12 @@ import {
 
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {BookRouterModule} from './book-router.module';
+import {RatingModule} from 'ng2-rating';
 
 import {ListComponent} from './list/list.component';
 import {AddComponent} from './add/add.component';
 import {BookComponent} from './book.component';
-import { DetailsComponent } from './details/details.component';
+import {DetailsComponent} from './details/details.component';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { DetailsComponent } from './details/details.component';
     MatInputModule,
     MdButtonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RatingModule
   ],
   declarations: [
     BookComponent,
