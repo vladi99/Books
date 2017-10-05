8d1d3f417aed6d0cdfef0966c85db0d3666e0b5c
vladi99 <vlpetrovic99@gmail.com>
2017-10-05 21:10:20 +0300

    add edit form

--- src/app/book/edit/edit.component.ts
+++ src/app/book/edit/edit.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {MD_DIALOG_DATA, MdDialogRef, MdSnackBar} from '@angular/material';
 import {BookService} from '../../services/book.service';
 import {Book} from '../../models/book.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-book',
@@ -10,10 +11,36 @@ import {Book} from '../../models/book.model';
 })
 export class EditComponent {
 
-  constructor(public dialogRef: MdDialogRef<EditComponent>,
+  bookForm: FormGroup;
+
+  minPrice = 1;
+  maxPrice = 100;
+
+  minRating = 1;
+  maxRating = 5;
+
+  constructor(@Inject(FormBuilder) fb: FormBuilder,
+              public dialogRef: MdDialogRef<EditComponent>,
               @Inject(MD_DIALOG_DATA) public book: Book,
               private bookService: BookService,
               public snackBar: MdSnackBar) {
+
+    this.bookForm = fb.group({
+      name: [this.book.Name, Validators.required],
+      author: [this.book.Author, Validators.required],
+      price: [this.book.Price, [
+        Validators.required,
+        Validators.min(this.minPrice),
+        Validators.max(this.maxPrice)]
+      ],
+      rating: [this.book.Rating, [
+        Validators.required,
+        Validators.min(this.minRating),
+        Validators.max(this.maxRating)]
+      ],
+      pictureUrl: this.book.PictureURL,
+      description: this.book.Description
+    });
   }
 
   onCloseClick(): void {
